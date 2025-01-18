bfa94deb00ee2d7de6d33f744b150b15a669ec38
sobereodg <soberegoodluck8@gmail.com>
2025-01-18 16:25:31 +0100

    created journal

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry()
+    {
+        
+    }
+
+    public void DisplayJournal()
+    {
+
+    }
+
+    public void SaveToFile()
+    {
+
+    }
+
+    public void LoadFromFile()
+    {
+
+    }
+}
