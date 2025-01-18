117e56f290454662ed2a633e640fd018f92589ea
sobereodg <soberegoodluck8@gmail.com>
2025-01-18 23:28:21 +0100

    generator updated

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -6,7 +6,9 @@ public class PromptGenerator
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "Made anyone happy today?",
+        "How can you explain your day"
     };
 
     public string GetRandomPrompt()
