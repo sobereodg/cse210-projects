784a29e6f599bbc48ea857a793c687e4e4d0159c
sobereodg <soberegoodluck8@gmail.com>
2025-01-18 22:19:07 +0100

    Promptgenerator updated

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,17 @@
+public class PromptGenerator
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        return _prompts[random.Next(_prompts.Count)];
+    }
+}
