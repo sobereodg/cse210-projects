bb1c2a050beef4ce8000c903c1e757d53d1da46d
sobereodg <soberegoodluck8@gmail.com>
2025-01-18 23:26:23 +0100

    program updated

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        Journal journal = new Journal();
+        string userChoice = "";
+
+        while (userChoice != "5")
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("Choose an option: ");
+            userChoice = Console.ReadLine();
+
+            // Process user choice
+            switch (userChoice)
+            {
+                case "1": 
+                    journal.AddEntry();
+                    break;
+                case "2": 
+                    journal.DisplayJournal();
+                    break;
+                case "3": 
+                    journal.SaveToFile();
+                    break;
+                case "4":
+                    journal.LoadFromFile();
+                    break;
+                case "5":
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }       
     }
 }
